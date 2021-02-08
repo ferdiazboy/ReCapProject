161ce6f1eb9bc4570b1778c2316a78f9e671cd51
ferdiazboy <ferdiazboy@gmail.com>
2021-02-08 23:56:15 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,86 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            //Listedeki tüm araçları sıralama
+            Console.WriteLine("---Galerideki Araçlar---\n");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + "-" + car.Description + " " + car.DailyPrice);
+            }
+            Console.WriteLine();
+
+            //Kullanıcıdan alınan Id'ye göre araç sıralam
+            Console.WriteLine("---Id'ye göre araç sıralama--- ");
+            Console.Write("Araç no giriniz(1-4):");
+            int id = Convert.ToInt32(Console.ReadLine());
+            foreach (var car in carManager.GetById(id))
+            {
+                Console.WriteLine(car.Description + " " + car.DailyPrice);
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
+
+            //Araç Ekleme
+            Console.WriteLine("---Araç Ekleme---");
+            System.Threading.Thread.Sleep(3000);
+            Car car1 = new Car()
+            {
+                Id = 5,
+                BrandId = 5,
+                ColorId = 3,
+                DailyPrice = 500,
+                ModelYear = 2020,
+                Description = "Audi A4-Otomatik"
+            };
+            carManager.Add(car1);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + "-" + car.Description + " " + car.DailyPrice);
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
+
+            // Araç Güncelleme
+            Console.WriteLine("---Araç Güncelleme(1 Nolu)---");
+            System.Threading.Thread.Sleep(3000);
+            Car car2 = new Car()
+            {
+                Id = 1,
+                BrandId = 6,
+                ColorId = 2,
+                DailyPrice = 400,
+                ModelYear = 2021,
+                Description = "Honda Civic-Otomatik"
+            };
+            carManager.Update(car2);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + "-" + car.Description + " " + car.DailyPrice);
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
+
+
+            // Araç Silme 
+            Console.WriteLine("---Araç Silme(5 Nolu)---");
+            System.Threading.Thread.Sleep(3000);
+            carManager.Delete(car1);
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + "-" + car.Description + " " + car.DailyPrice);
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { Id = 1, BrandId = 1, ColorId = 1, DailyPrice = 250, Description = "Renault Symbol-Manuel", ModelYear = 2020 },
+                new Car { Id = 2, BrandId = 2, ColorId = 1, DailyPrice = 200, Description = "Fiat Egea-Manuel", ModelYear = 2020 },
+                new Car { Id = 3, BrandId = 3, ColorId = 2, DailyPrice = 350, Description = "Wolkswagen Golf-Otomatik", ModelYear = 2021 },
+                new Car { Id = 4, BrandId = 4, ColorId = 2, DailyPrice = 500, Description = "Nissan Qasqai-Otomatik", ModelYear = 2021 },
+            };
+           
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
